9b0c70505ccaead99f81db2ccd27eb24404832da
もーりす <ymo999@users.noreply.github.com>
2020-08-03 21:34:24 +0900

    20200803

--- 2020/20200802_ABC174/ABC174_C_ex.py
+++ 2020/20200802_ABC174/ABC174_C_ex.py
@@ -1,18 +1,22 @@
+'''
+参考
+https://qiita.com/c-yan/items/d1b19114d411755f25b5
+https://qiita.com/easychoco/items/7f1f00d9221bd3ab0a6c
+数列は以下で表すことができる
+a[n+1] = a[n] * 10 + 7
+a[n] % K == 0となれば、その時点でのnが解となる
+'''
 K = int(input())
-
 if K % 2 == 0:
     print(-1)
     exit()
 
-# Kの桁数
-K_str = str(K)
-K_len = len(K_str)
+num = 0
 
-for i in range(K_len, K):
-    num_str = '7' * i
-    num = int(num_str)
-    if num % K == 0:
-        print(i)
+for i in range(K):
+    num = (num * 10 + 7) % K
+    if num == 0:
+        print(i+1)
         exit()
 
 print(-1)
--- 2020/20200802_ABC174/ABC174_D_ex.py
+++ 2020/20200802_ABC174/ABC174_D_ex.py
@@ -1,17 +1,24 @@
+'''
+参考
+https://qiita.com/tanakh/items/a312a9bd684658ab1e7b
+左側から「白」が見つかったら、右側から見つかった「赤」と入れ替える
+'''
 N = int(input())
 C = list(map(str, input()))
 
+left = 0
+right = N - 1
 ans = 0
 
-for i in range(N-1):
-    if C[i] == 'W' and C[i+1] == 'R':
-        tmp = C[i]
-        C[i] = C[i+1]
-        C[i+1] = tmp
-        ans += 1
-
-if C[N-2] == 'W' and C[N-1] == 'R':
-    C[N-1] == 'W'
-    ans += 1
+while True:
+    while left < N and C[left] != 'W':      # 左側から「白」が見つかるまでポインタをずらしていく
+        left += 1
+    while right >= 0 and C[right] != 'R':   # 右側から「赤」が見つかるまでポインタをずらしていく
+        right -= 1
+    if left >= right:                       # 停止条件
+        break
+    ans += 1                                # 見つかったら解をカウントアップ（赤白入れ替え）
+    left += 1
+    right -= 1
 
 print(ans)
